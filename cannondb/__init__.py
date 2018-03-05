0dfd717a0246d92310b7c5c1074a3711133edf55
Cqk <cqk0100@gmail.com>
2018-03-05 19:15:50 +0800

    complete setup.py

--- cannondb/__init__.py
+++ cannondb/__init__.py
@@ -1,6 +1,8 @@
 from cannondb.database import CannonDB
 from cannondb.storages import FileStorage, MemoryStorage
 
+__version__ = '1.0.0'
+
 __all__ = ('CannonDB', 'FileStorage', 'MemoryStorage')
 
 
--- setup.py
+++ setup.py
@@ -1,27 +1,60 @@
+import codecs
 import os
+import re
 
 from setuptools import setup
 
 
-def read(f_name):
-    path = os.path.join(os.path.dirname(__file__), f_name)
-    return open(path, encoding='utf-8')
+def open_local(paths: list, mode='r', encoding='utf-8'):
+    path = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        *paths
+    )
+
+    return codecs.open(path, mode, encoding)
 
 
-with read('README.md') as f:
+with open_local(['README.rst']) as f:
     long_description = f.read()
 
-with read('cannondb/version.py') as f:
-    version = f.read()
+with open_local(['cannondb', '__init__.py']) as fp:
+    try:
+        version = re.findall(r"^__version__ = '([^']+)'\r?$",
+                             fp.read(), re.M)[0]
+    except IndexError:
+        raise RuntimeError('Unable to determine version.')
 
 setup(
     name='cannondb',
     version=version,
     packages=['cannondb'],
+    zip_safe=True,
     url='https://github.com/SimonCqk/cannondb',
     license='MIT',
     author='SimonCqk',
     author_email='cqk0100@gmail.com',
     description='CannonDB is a lightweight but powerful key-value database created for human beings.',
-    install_requires=['rwlock']
+    keywords='database key-value python nosql',
+    long_description=long_description,
+    install_requires=[
+        'rwlock'
+    ],
+    platforms='any',
+    python_requires=">=3.5",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3 :: Only',
+        'Natural Language :: English',
+        'Topic :: Database',
+        'Topic :: Communications :: Email',
+        'Topic :: Database :: Database Engines/Servers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+    ]
 )
