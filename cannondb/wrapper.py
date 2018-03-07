8263ae92fec36dc2428373bb2ce8b5b91da1c3fd
Cqk <cqk0100@gmail.com>
2018-03-07 20:32:47 +0800

    add first impl of log wrapper.

--- cannondb/wrapper.py
+++ cannondb/wrapper.py
@@ -1,6 +1,28 @@
 """
   This file include storage wrappers to expand the functionality of storage.
-- StorageWrapper: Abstract base wrapper.
-- CacheWrapper: Use cache to improve performance.
-- LogWrapper: Use logs to record all pivotal operations.
+- log_wrapper: Use logs to record all pivotal operations.
 """
+import datetime
+import logging
+import os
+
+
+def log_wrapper(cls, methods_to_log: tuple = ('add', 'decrease'), config='local'):
+    logger = logging.getLogger(cls.__class__.__name__)
+    if config == 'socket':
+        pass
+    else:
+        if not os.path.exists('log.log'):
+            os.open('log.log', os.O_RDWR | os.O_CREAT)
+        logger.addHandler(logging.FileHandler('log.log', mode='w+'))
+
+    orig_getattr = cls.__class__.__getattribute__
+
+    def _wrapper(self, name):
+        if name in methods_to_log:
+            logger.warning('{cls} {method} at {time}'.format(cls=cls.__class__.__name__, method=name,
+                                                             time=datetime.datetime.now().isoformat()))
+        return orig_getattr(self, name)
+
+    cls.__class__.__getattribute__ = _wrapper
+    return cls
