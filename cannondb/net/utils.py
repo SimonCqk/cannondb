99bcf27ad39e432be151f8cff7982d89acaff518
Cqk <cqk0100@gmail.com>
2018-06-21 18:17:28 +0800

    add necessary comments 1.0!

--- cannondb/net/utils.py
+++ cannondb/net/utils.py
@@ -16,10 +16,10 @@ def redis_manual_launcher():
         # os.system() will block the whole program, so avoid to use it.
         subprocess.Popen('redis-server', shell=True, stdout=subprocess.PIPE)
         '''
-        guarantee the server has started, since "Popen" execute cmd asynchronously,
-        I'm not sure if sleep is necessary.
+        guarantee the server has started, since sub process module executes cmd asynchronously,
+        I'm not sure whether `sleep` is necessary or not.
         '''
-        time.sleep(0.001)
+        time.sleep(0.01)
         if not has_redis_launched():
             raise RedisNotLaunchedError('redis server cannot launch properly, please check the configuration')
 
