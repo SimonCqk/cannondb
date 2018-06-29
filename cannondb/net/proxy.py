9998faa3e1ec4ce4322580148e928e6605a483e5
Cqk <cqk0100@gmail.com>
2018-06-29 23:50:11 +0800

    refine private key response process.

--- cannondb/net/proxy.py
+++ cannondb/net/proxy.py
@@ -1,13 +1,19 @@
-from abc import ABCMeta
+import socket
+from abc import ABCMeta, abstractmethod
 
-from cannondb.net.utils import AESCipher
+from cannondb.net.utils import AESCipher, AES_KEY_LEN
+from cannondb.serializer import StrSerializer
 
 
 class AbstractProxy(metaclass=ABCMeta):
+    BUFFER_SIZE = 2048
+    KEY_BUFFER_SIZE = AES_KEY_LEN
 
+    @abstractmethod
     def send(self, msg: bytes):
         pass
 
+    @abstractmethod
     def receive_from(self):
         pass
 
@@ -16,6 +22,7 @@ class ClientProxy(AbstractProxy):
 
     def __init__(self):
         self._server_key = None
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def send(self, msg: bytes):
         pass
@@ -23,11 +30,28 @@ class ClientProxy(AbstractProxy):
     def receive_from(self):
         pass
 
-    def get_key_from_cipher(self, key: str):
-        self._server_key = key
+    def get_key_from_server(self):
+        """
+        Receive key sent from server socket. It should be done instantly when client connects
+        to the server successfully, server send its private key as response.
+        """
+        key = self._sock.recv(self.KEY_BUFFER_SIZE)
+        assert len(key) == self.KEY_BUFFER_SIZE
+        # deserialize key in bytes to string
+        self._server_key = StrSerializer.deserialize(key)
 
 
 class ServerProxy(AbstractProxy):
 
     def __init__(self):
         self._aes = AESCipher()
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def send(self, msg: bytes):
+        pass
+
+    def receive_from(self):
+        pass
+
+    def _respond_key(self, cli: ClientProxy):
+        self._aes.transmit_key_to_client(cli)
